6ec9941c09e890596434338fb1cc03b99fb18e41
Raj Gohire <70620767+RajGohire@users.noreply.github.com>
2023-08-05 23:45:07 +0100

    Setup

--- resume-app/src/App.js
+++ resume-app/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from "./Components/Footer";
+import Spline from "@splinetool/react-spline";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+			<body>
+				{/* <Spline scene="https://prod.spline.design/JjFHTdgK6W2e2Oap/scene.splinecode" /> */}
+			</body>
+			<footer>
+				<Footer />
+			</footer>
+		</div>
+	);
 }
 
 export default App;
